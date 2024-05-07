e543060e6d496218cd50eb32e0622cb26f995b39
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-06 22:14:20 -0400

    fix: src/main.tsx(3,10): error TS2614: Module './App'

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,11 @@
 import { Attribute } from "./Attribute";
 import "./App.css";
 
-function App() {
+export const App = () => {
   return (
     <>
       <h1>Hello World</h1>
       <Attribute />
     </>
   );
-}
-
-export default App;
+};
