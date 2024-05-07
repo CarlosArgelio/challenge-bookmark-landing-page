2efb191530ae8792a2703438e493531cd73b4182
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-06 22:10:59 -0400

    feat: configuration project

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Attribute } from "./Attribute";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Hello World</h1>
+      <Attribute />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Attribute.tsx
@@ -0,0 +1,13 @@
+export const Attribute = () => {
+  return (
+    <footer>
+      <p className="attribution">
+        Challenge by
+        <a href="https://www.frontendmentor.io?ref=challenge" target="_blank">
+          Frontend Mentor
+        </a>
+        . Coded by <a href="#">Carlos A. Palacios R.</a>.
+      </p>
+    </footer>
+  );
+};
