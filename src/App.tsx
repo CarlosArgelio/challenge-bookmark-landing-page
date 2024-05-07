de7ec8b28da237cf7e0f50763456678464a149b8
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-06 23:03:17 -0400

    style: add config colors & font family

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,9 @@ import { Attribute } from "./Attribute";
 export const App = () => {
   return (
     <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <h1 className="text-3xl font-bold font-Rubik text-soft-blue underline">
+        A Simple Bookmark Manager
+      </h1>
       <Attribute />
     </>
   );
