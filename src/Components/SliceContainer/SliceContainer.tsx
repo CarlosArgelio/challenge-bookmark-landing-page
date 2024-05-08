8155a8fe45bb8166fec7240d40a323733198b4dd
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-08 12:14:49 -0400

    feat: add state to componenet slice

--- src/Components/SliceContainer/SliceContainer.tsx
+++ src/Components/SliceContainer/SliceContainer.tsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import { Slice } from "./../../Components";
 import { svg } from "./../../assets";
 
@@ -8,26 +9,49 @@ const styles = {
 };
 
 export const SliceContainer = () => {
+  const [viewSimpleBooking, setViewSimpleBooking] = useState<boolean>(true);
+  const [viewSpeedySearching, setViewSpeedySearching] =
+    useState<boolean>(false);
+  const [viewEasySharing, setViewEasySharing] = useState<boolean>(false);
+
+  const handleSimpleBooking = () => {
+    setViewSimpleBooking(true);
+    setViewSpeedySearching(false);
+    setViewEasySharing(false);
+  };
+
+  const handleSpeedySearching = () => {
+    setViewSimpleBooking(false);
+    setViewSpeedySearching(true);
+    setViewEasySharing(false);
+  };
+
+  const handleEasySharing = () => {
+    setViewSimpleBooking(false);
+    setViewSpeedySearching(false);
+    setViewEasySharing(true);
+  };
+
   return (
     <>
       <div className="mb-8">
         <div className="mb-10 mx-5">
-          <div className="hidden">
+          <div className={`${viewSimpleBooking ? "" : "hidden"}`}>
+            <Slice
+              title="Features"
+              description="Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go."
+            />
+          </div>
+          <div className={`${viewSpeedySearching ? "" : "hidden"}`}>
             <Slice
               title="Intelligent search"
               description="Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks."
             />
           </div>
-          <div className="hidden">
+          <div className={`${viewEasySharing ? "" : "hidden"}`}>
             <Slice
               title="Share your bookmarks"
               description="Our powerful search feature will help you find saved sites in no time at all. No need to trawl through all of your bookmarks."
-            />
-          </div>
-          <div className="">
-            <Slice
-              title="Features"
-              description="Our aim is to make it quick and easy for you to access your favourite websites. Your bookmarks sync between your devices so you can access them on the go."
             />
           </div>
         </div>
@@ -35,10 +59,17 @@ export const SliceContainer = () => {
           <div className={`${styles.lineGray}`} />
 
           <div>
-            <button className="w-[302px] mt-4 mb-4 text-base text-very-dark-blue text-opacity-90 text-center">
+            <button
+              onClick={handleSimpleBooking}
+              className="w-[302px] mt-4 mb-4 text-base text-very-dark-blue text-opacity-90 text-center"
+            >
               Simple Bookmarking
             </button>
-            <div className="flex justify-center items-center">
+            <div
+              className={`flex justify-center items-center ${
+                viewSimpleBooking ? "" : "hidden"
+              }`}
+            >
               <img src={icons["icon-rectangle-red"]} alt="" />
             </div>
           </div>
@@ -46,20 +77,34 @@ export const SliceContainer = () => {
           <div className={`${styles.lineGray}`} />
 
           <div>
-            <button className="w-[302px] mt-4 mb-4 text-base text-very-dark-blue text-opacity-90 text-center">
+            <button
+              onClick={handleSpeedySearching}
+              className="w-[302px] mt-4 mb-4 text-base text-very-dark-blue text-opacity-90 text-center"
+            >
               Speedy Searching
             </button>
-            <div className="flex justify-center items-center">
+            <div
+              className={`flex justify-center items-center ${
+                viewSpeedySearching ? "" : "hidden"
+              }`}
+            >
               <img src={icons["icon-rectangle-red"]} alt="" />
             </div>
           </div>
           <div className={`${styles.lineGray}`} />
 
           <div>
-            <button className="w-[302px] mt-4 mb-4 text-base text-very-dark-blue text-opacity-90 text-center">
+            <button
+              onClick={handleEasySharing}
+              className="w-[302px] mt-4 mb-4 text-base text-very-dark-blue text-opacity-90 text-center"
+            >
               Easy Sharing
             </button>
-            <div className="flex justify-center items-center">
+            <div
+              className={`flex justify-center items-center ${
+                viewEasySharing ? "" : "hidden"
+              }`}
+            >
               <img src={icons["icon-rectangle-red"]} alt="" />
             </div>
           </div>
