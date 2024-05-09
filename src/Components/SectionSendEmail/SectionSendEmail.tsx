ec34b0970580dc1f77896635d932935835644eb7
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-09 13:15:25 -0400

    style: section send email

--- src/Components/SectionSendEmail/SectionSendEmail.tsx
+++ src/Components/SectionSendEmail/SectionSendEmail.tsx
@@ -2,13 +2,22 @@ import { ButtonPrimary } from "./../../Components";
 
 export const SectionSendEmail = () => {
   return (
-    <div className="bg-soft-blue">
-      <p>35,000+ already joined</p>
-      <h2>Stay up-to-date with what we’re doing</h2>
+    <div className="bg-soft-blue h-[360px]">
+      <div className="flex flex-col items-center justify-center mx-5">
+        <div className="text-white text-center mt-16">
+          <p className="text-sm tracking-widest mb-4">35,000+ already joined</p>
+          <h2 className="text-2xl mb-8">
+            Stay up-to-date with what we’re doing
+          </h2>
+        </div>
 
-      <input type="email" placeholder="Enter your email address" />
-
-      <ButtonPrimary type="typeOne" children="Contact Us" />
+        <div className="mb-4">
+          <input type="email" placeholder="Enter your email address" />
+        </div>
+        <div className="absolute mt-[350px]">
+          <ButtonPrimary type="typeOne" children="Contact Us" />
+        </div>
+      </div>
     </div>
   );
 };
