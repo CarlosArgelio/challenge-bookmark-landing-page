426d88a288a8c7a5e3d232477b30d8968e3f3f26
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-09 12:59:56 -0400

    feat: add footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,16 +5,22 @@ const icons = svg.icons;
 
 export const Footer = () => {
   return (
-    <footer className="bg-very-dark-blue">
-      <img src={logos["logo-bookmark-bg-text-white"]} alt="logo bookmark" />
+    <footer className="bg-very-dark-blue h-[334px]">
+      <div className="flex flex-col items-center justify-center p-5">
+        <div className="mb-10">
+          <img src={logos["logo-bookmark-bg-text-white"]} alt="logo bookmark" />
+        </div>
 
-      <p>Features</p>
-      <p>Pricing</p>
-      <p>Contact</p>
+        <div className="flex flex-col items-center justify-center gap-8 mb-10 text-white tracking-wider">
+          <p>Features</p>
+          <p>Pricing</p>
+          <p>Contact</p>
+        </div>
 
-      <div>
-        <img src={icons["icon-facebook"]} alt="icon facebook" />
-        <img src={icons["icon-twitter"]} alt="icon twitter" />
+        <div className="flex gap-10">
+          <img src={icons["icon-facebook"]} alt="icon facebook" />
+          <img src={icons["icon-twitter"]} alt="icon twitter" />
+        </div>
       </div>
     </footer>
   );
