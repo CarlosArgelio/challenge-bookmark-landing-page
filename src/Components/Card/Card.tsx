5c08f76402a6fe8e2f869f0293468fe3ca50f341
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-11 19:54:21 -0400

    style: add responsive 768px to Card Container

--- src/Components/Card/Card.tsx
+++ src/Components/Card/Card.tsx
@@ -3,11 +3,13 @@ import { ButtonPrimary, P } from "./../../Components";
 
 export interface Prop {
   logo: string;
+  title: string;
+  version: string;
 }
 
 const utils = svg.utils;
 
-export const Card = ({ logo }: Prop) => {
+export const Card = ({ logo, title, version }: Prop) => {
   return (
     <>
       <div className="flex flex-col items-center mb-10 w-[280px] h-[371px] mx-12 rounded-2xl shadow-lg">
@@ -15,8 +17,8 @@ export const Card = ({ logo }: Prop) => {
           <img src={logo} alt="Icon" />
         </div>
         <div className="mb-6">
-          <h3 className="text-xl text-center font-medium">Title</h3>
-          <P>Version Soported</P>
+          <h3 className="text-xl text-center font-medium">{title}</h3>
+          <P>{version}</P>
         </div>
 
         <img className="mb-3" src={utils["bg-dots"]} alt="Icon" />
--- src/Components/CardContainer/CardContainer.tsx
+++ src/Components/CardContainer/CardContainer.tsx
@@ -5,16 +5,31 @@ const logos = svg.logos;
 
 export const CardContainer = () => {
   return (
-    <div className="mb-36">
-      <H2>Download the extension</H2>
-      <P>
-        We’ve got more browsers in the pipeline. Please do let us know if you’ve
-        got a favourite you’d like us to prioritize.
-      </P>
-
-      <Card logo={logos["logo-chrome"]} />
-      <Card logo={logos["logo-firefox"]} />
-      <Card logo={logos["logo-opera"]} />
+    <div className="mb-36 md:mx-14">
+      <div>
+        <H2>Download the extension</H2>
+        <P>
+          We’ve got more browsers in the pipeline. Please do let us know if
+          you’ve got a favourite you’d like us to prioritize.
+        </P>
+      </div>
+      <div className="flex items-center flex-col">
+        <Card
+          logo={logos["logo-chrome"]}
+          title="Add to Chrome"
+          version="Minimum version 62"
+        />
+        <Card
+          logo={logos["logo-firefox"]}
+          title="Add to Firefox"
+          version="Minimum version 55"
+        />
+        <Card
+          logo={logos["logo-opera"]}
+          title="Add to Opera"
+          version="Minimum version 46"
+        />
+      </div>
     </div>
   );
 };
