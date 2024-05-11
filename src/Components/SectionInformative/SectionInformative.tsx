50ab814d93532b262eb18f46e0b5d51a75825fef
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-11 19:47:52 -0400

    style: add responsive 768px to Section informative

--- src/Components/SectionInformative/SectionInformative.tsx
+++ src/Components/SectionInformative/SectionInformative.tsx
@@ -6,21 +6,7 @@ const icons = svg.icons;
 
 export const SectionInformative = () => {
   return (
-    <div className="flex flex-col-reverse">
-      <div className="mx-5 mt-8">
-        <H2>Bookmark in one click</H2>
-        <div>
-          <P>
-            Organize your bookmarks however you like. Our simple drag-and-drop
-            interface gives you complete control over how you manage your
-            favourite sites.
-          </P>
-        </div>
-
-        <div className="flex justify-center mb-36">
-          <ButtonPrimary type="typeOne" children="More Info" />
-        </div>
-      </div>
+    <div className="flex flex-col md:flex-row">
       <div className="relative mb-12">
         <div className="absolute top-[2.2rem] left-0 -z-50">
           <img src={icons["icon-rectangle-left"]} alt="icon rectangle" />
@@ -30,6 +16,20 @@ export const SectionInformative = () => {
             src={ilustration["illustration-features-tab-1"]}
             alt="illustration hero"
           />
+        </div>
+      </div>
+      <div className="mx-5 mt-8">
+        <H2 styles="md:text-start md:text-2xl">Bookmark in one click</H2>
+        <div>
+          <P styles="md:text-xs md:text-start">
+            Organize your bookmarks however you like. Our simple drag-and-drop
+            interface gives you complete control over how you manage your
+            favourite sites.
+          </P>
+        </div>
+
+        <div className="flex justify-center md:justify-start mb-36">
+          <ButtonPrimary type="typeOne" children="More Info" />
         </div>
       </div>
     </div>
