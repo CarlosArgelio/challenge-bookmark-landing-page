afae1ca54ac0fec6c46f79694f85088cd2cba0a5
Carlos A Palacios R <carlosargelio0104@gmail.com>
2024-05-11 20:16:51 -0400

    style: add responsive 768px to Footer

--- src/Components/Footer/Footer.tsx
+++ src/Components/Footer/Footer.tsx
@@ -5,16 +5,16 @@ const icons = svg.icons;
 
 export const Footer = () => {
   return (
-    <footer className="bg-very-dark-blue h-[334px]">
-      <div className="flex flex-col items-center justify-center p-5">
-        <div className="mb-10">
+    <footer className="bg-very-dark-blue h-[334px] md:h-full">
+      <div className="flex flex-col items-center justify-center p-5 md:flex-row md:p-0">
+        <div className="mb-10 md:m-5">
           <img src={logos["logo-bookmark-bg-text-white"]} alt="logo bookmark" />
         </div>
 
-        <div className="flex flex-col items-center justify-center gap-8 mb-10 text-white tracking-wider">
-          <p>Features</p>
-          <p>Pricing</p>
-          <p>Contact</p>
+        <div className="flex flex-col items-center justify-center gap-8 mb-10 text-white tracking-wider md:flex-row md:m-5">
+          <p className="md:hover:text-soft-red">Features</p>
+          <p className="md:hover:text-soft-red">Pricing</p>
+          <p className="md:hover:text-soft-red">Contact</p>
         </div>
 
         <div className="flex gap-10">
